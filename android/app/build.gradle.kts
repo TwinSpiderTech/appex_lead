394ec5facc3fc5ec688adf5994286d2fe3fcbff3
Mosen <muhammad.mohsin@twinspider.com>
2026-04-22 15:26:00 +0500

    ..

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.twinspider.appex_lead"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.2"
     }
 
